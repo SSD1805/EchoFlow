dd0d4f46a6b77620f63b788d9829470cc5686101
SSD1805 <44541455+SSD1805@users.noreply.github.com>
2026-08-16 19:51:30 -0300

    export audio stream selector

--- src/echoflow/media/__init__.py
+++ src/echoflow/media/__init__.py
@@ -1,9 +1,11 @@
-"""Local media inspection capability."""
+"""Local media inspection and stream-selection capabilities."""
 
 from echoflow.media.models import InputIdentity, MediaInfo, MediaStream, StreamKind
 from echoflow.media.probe import FfprobeMediaProbe
+from echoflow.media.selection import AudioStreamSelector
 
 __all__ = [
+    "AudioStreamSelector",
     "FfprobeMediaProbe",
     "InputIdentity",
     "MediaInfo",
